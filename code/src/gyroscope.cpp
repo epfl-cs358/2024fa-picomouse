5f6a6717dbfb5ac000ea87408c76b6a9040f59d5
zperego <zperego@balelec.ch>
2024-12-02 19:32:54 +0100

    bref

--- code/src/gyroscope.cpp
+++ code/src/gyroscope.cpp
@@ -1,66 +1,75 @@
 #include "DFRobot_BMI160.h"
+#include "gyroscope.h"
 #include <math.h>
 
+#define NS_TO_S 1000000
+
 double angle = 0.0;
-DFRobot_BMI160 bmi160; 
+DFRobot_BMI160 bmi160;
+const int8_t i2c_addr = 0x68;
 
-void reset_angle(){
-    angle = 0.0;
+void reset_angle() {
+  angle = 0.0;
 }
 
-double get_angle(int16_t angular_speed, double delta_time){
-   return angle;
+double get_angle(int16_t angular_speed, double delta_time) {
+  return angle;
 }
 
-RESULT update_gyro(double threshold){
-    double mean_val = 0;
-    int N = 10;
-    int t = 10;
-    int8_t rslt;
-    int16_t angularSpeeds[6]={0};
-    uint32_t time_stamp[2]= {0};
-    uint32_t start_time = 0;
-    uint32_t end_time = 0;
+RESULT update_gyro(double threshold) {
+  double mean_val = 0;
+  int N = 10;
+  int t = 10;
+  int8_t rslt;
+  int16_t angularSpeeds[6] = { 0 };
+  uint32_t time_stamp[2] = { 0 };
+  uint32_t start_time = 0;
+  uint32_t end_time = 0;
 
-    for(int i=0; i<N; i++){
+  for (int i = 0; i < N; i++) {
 
-        rslt = bmi160.getAccelGyroData(angularSpeeds, time_stamp);
+    rslt = bmi160.getAccelGyroData(angularSpeeds, time_stamp);
 
-        if (rslt != 0) {
-          Serial.println("I fucking died mate");
-          while (1);
-        }
-        if (i == 0){
-          start_time = time_stamp[0];
-        }
-        mean_val += static_cast<double>(angularSpeeds[2]);
-      }
-      end_time = time_stamp[0];
-      double elapsed_time_ns = (end_time - start_time);
-      mean_val /= N;
-      mean_val *= 3.14 / 180.0;
-
-      if(((mean_val * elapsed_time_ns)/NS_TO_S > threshold) || ((mean_val * elapsed_time_ns)/NS_TO_S < -threshold)){
-        angle += (mean_val * elapsed_time_ns)/NS_TO_S;
-      }
-
-    angle = fmod(angle + 1, 2); // Shift by 1, then take modulo 2
-    if (angle < 0) {
-        angle += 2; // Ensure positive result
+    if (rslt != 0) {
+      Serial.println("I fucking died mate");
+      while (1)
+        ;
     }
-    angle -= 1; // Shift back to [-1, 1]
+    if (i == 0) {
+      start_time = time_stamp[0];
+    }
+    mean_val += static_cast<double>(angularSpeeds[2]);
+  }
+  end_time = time_stamp[0];
+  double elapsed_time_ns = (end_time - start_time);
+  mean_val /= N;
+  mean_val *= 3.14 / 180.0;
+
+  if (((mean_val * elapsed_time_ns) / NS_TO_S > threshold) || ((mean_val * elapsed_time_ns) / NS_TO_S < -threshold)) {
+    angle += (mean_val * elapsed_time_ns) / NS_TO_S;
+  }
+
+  angle = fmod(angle + 1, 2);  // Shift by 1, then take modulo 2
+  if (angle < 0) {
+    angle += 2;  // Ensure positive result
+  }
+  angle -= 1;  // Shift back to [-1, 1]
+  return NO_ERROR;
 }
 
-RESULT setup_gyro(){
-    //init the hardware bmin160
-  if (bmi160.softReset() != BMI160_OK){
+RESULT setup_gyro() {
+  //init the hardware bmin160
+  if (bmi160.softReset() != BMI160_OK) {
     Serial.println("reset false");
-    while(1);
+    while (1)
+      ;
   }
 
   //set and init the bmi160 i2c address
-  if (bmi160.I2cInit(i2c_addr) != BMI160_OK){
+  if (bmi160.I2cInit(i2c_addr) != BMI160_OK) {
     Serial.println("init false");
-    while(1);
+    while (1)
+      ;
   }
+  return NO_ERROR;
 }
--- code/test_gyro/test_gyro_motor.cpp
+++ code/test_gyro/test_gyro_motor.cpp
@@ -7,7 +7,7 @@
 #include "motion.h"
 
 
-#define CORRECTION_ANGLE 1/180 * 5
+#define CORRECTION_ANGLE 1 / 180 * 5
 
 
 const int8_t i2c_addr = 0x68;
@@ -21,29 +21,29 @@ double offset = 0.0;
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define NB_MEASURES 4
 #define NS_TO_S 1000000
 double current_angle = 0;
 double target_angle = 0;
 
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-    };
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  };
 
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-    }
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
 };
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(100);
 
-    stop();
+  stop();
   setup_gyro();
   // Create the BLE Device
   BLEDevice::init("ESP_32_PICOMOUSE");
@@ -53,13 +53,12 @@ void setup() {
   pServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLEService* pService = pServer->createService(SERVICE_UUID);
 
   // Create a BLE Characteristic
   pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID, //                      BLECharacteristic::PROPERTY_READ   |BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_INDICATE
-                      BLECharacteristic::PROPERTY_NOTIFY 
-                    );
+    CHARACTERISTIC_UUID,  //                      BLECharacteristic::PROPERTY_READ   |BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_INDICATE
+    BLECharacteristic::PROPERTY_NOTIFY);
 
   // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
   // Create a BLE Descriptor
@@ -69,7 +68,7 @@ void setup() {
   pService->start();
 
   // Start advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
@@ -78,59 +77,52 @@ void setup() {
 }
 
 void loop() {
-      RESULT rslt_init_pos = NO_ERROR;
-      rslt_init_pos = init_pos(&bmi160, &offset);
 
-      
-          // notify changed value
-        if (deviceConnected) {
-            int i = 0;
-            int rslt;
-            int16_t accelGyro[6] = {0};
-            uint32_t time_stamp[2]= {0};
-            uint32_t start_time = 0;
-            uint32_t end_time = 0;
-            float value = 0;
 
-        // Get both accel and gyro data from BMI160
-        // parameter accelGyro is the pointer to store the data
-            update_gyro(0.00150);
+  // notify changed value
+  if (deviceConnected) {
+    int i = 0;
+    int rslt;
+    int16_t accelGyro[6] = { 0 };
+    uint32_t time_stamp[2] = { 0 };
+    uint32_t start_time = 0;
+    uint32_t end_time = 0;
+    float value = 0;
 
-            double current_angle = get_angle();
-            if(current_angle > CORRECTION_ANGLE){
-                turn_left(INPLACE);
-            }
-            else if(current_angle < -CORRECTION_ANGLE){
-                turn_right(INPLACE);
-            }
-            else
-            {
-                break_wheels();
-            }
+    // Get both accel and gyro data from BMI160
+    // parameter accelGyro is the pointer to store the data
+    update_gyro(0.00150);
+
+    double current_angle = get_angle();
+    if (current_angle > CORRECTION_ANGLE) {
+      turn_left(INPLACE);
+    } else if (current_angle < -CORRECTION_ANGLE) {
+      turn_right(INPLACE);
+    } else {
+      break_wheels();
+    }
 
 
 
 
-    
-          char buffer [200] = {0};
-          sprintf(buffer, "vz : %f",current_angle);
-          pCharacteristic->setValue((uint8_t*)buffer, strlen(buffer));
-          pCharacteristic->notify();
-          value++;
-          delay(10); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-        }
-        // disconnecting
-        if (!deviceConnected && oldDeviceConnected) {
-            delay(500); // give the bluetooth stack the chance to get things ready
-            pServer->startAdvertising(); // restart advertising
-            Serial.println("start advertising");
-            oldDeviceConnected = deviceConnected;
-        }
-        // connecting
-        if (deviceConnected && !oldDeviceConnected) {
-            // do stuff here on connecting
-            oldDeviceConnected = deviceConnected;
-        }
-      
-      
+
+    char buffer[200] = { 0 };
+    sprintf(buffer, "vz : %f", current_angle);
+    pCharacteristic->setValue((uint8_t*)buffer, strlen(buffer));
+    pCharacteristic->notify();
+    value++;
+    delay(10);  // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+  }
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500);                   // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising();  // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
 }
