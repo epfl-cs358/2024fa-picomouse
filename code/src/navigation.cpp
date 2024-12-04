bea6a886198265a821c1b4ade707a9ee9a5b1ea2
Sophia Kacem <sophia.kacem@gmail.com>
2024-12-04 09:35:10 +0100

    implementation of navigation on going

--- code/src/navigation.cpp
+++ code/src/navigation.cpp
@@ -1,2 +1,24 @@
 #include "navigation.h"
+#include "gyroscope.h"
 
+RESULT adjust_front_distance(){
+
+}
+
+RESULT adjust_sides_distance(){
+
+}
+
+RESULT alignement(){
+
+}
+
+RESULT turn(double angle, MODE mode){
+    while () {
+    
+    }
+}
+
+RESULT init_sensors(){
+
+}
